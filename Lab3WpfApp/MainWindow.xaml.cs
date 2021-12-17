485a728b60b60dce4ac8ca2ff1962d39f5987c57
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-12-17 15:10:00 +0300

    Добавьте файлы проекта.

---
+++ Lab3WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab3WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox !=null)
+            {
+                textBox.FontFamily = new FontFamily(fontName);
+            }
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string s = ((sender as ComboBox).SelectedItem as TextBlock).Text;
+            if (textBox1 != null)
+            {
+                textBox1.FontSize = double.Parse(s); //преобразование числа
+            }
+        }
+    }
+}

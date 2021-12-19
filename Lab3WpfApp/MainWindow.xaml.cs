dace6ff26273abffa58ba2bc5532e2a4b519b14e
VICTORIA TRINOSOVA <trinosova.victoria@yandex.ru>
2021-12-19 20:36:59 +0300

    1

--- Lab3WpfApp/MainWindow.xaml.cs
+++ Lab3WpfApp/MainWindow.xaml.cs
@@ -37,10 +37,46 @@ namespace Lab3WpfApp
         private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
             string s = ((sender as ComboBox).SelectedItem as TextBlock).Text;
-            if (textBox1 != null)
+            if (textBox != null)
             {
-                textBox1.FontSize = double.Parse(s); //преобразование числа
+                textBox.FontSize = double.Parse(s);
             }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.UltraBold)
+                textBox.FontWeight = FontWeights.Normal;
+            else
+                textBox.FontWeight =FontWeights.UltraBold;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (textBox.TextDecorations == TextDecorations.Underline)
+                textBox.TextDecorations = null;
+            else
+                textBox.TextDecorations = TextDecorations.Underline;
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Italic)
+                textBox.FontStyle = FontStyles.Normal;
+            else
+                textBox.FontStyle = FontStyles.Italic;
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Black;
+
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            textBox.Foreground = Brushes.Red;
         }
     }
 }
